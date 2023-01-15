0cd5fb53cd53c6614bef8e5f1691e2b3c98a636f
SpajroTheWolf <115029514+SpajroTheWolf@users.noreply.github.com>
2023-01-15 21:34:08 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -743,8 +743,8 @@ end)
 webhookTab:Button("Test", "Sends a test message to your webhook to verify it is working", function()
     if getgenv().settings.webhookBox then
     webhook("**Spajro's Donation Bot v2.0.3**")
-    webhook("```New Options:")
-    webhook("- Better Look Of Weebhook```")
+    webhook("``New Options:``")
+    webhook("``- Better Look Of Weebhook``")
     end
 end)
 
